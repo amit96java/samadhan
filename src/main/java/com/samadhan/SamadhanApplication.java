7d0d8639a837fb0ae8a0b6839d31efd12c662317
amit96pal <amit96pal.ap@gmail.com>
2023-04-02 10:08:31 +0530

    struction-1

---
+++ src/main/java/com/samadhan/SamadhanApplication.java
@@ -0,0 +1,13 @@
+package com.samadhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamadhanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamadhanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samadhan/SamadhanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samadhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamadhanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
